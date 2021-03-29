d9a6996cda17d91acb74558197b75f0235db2e68
satou <stuysyk1027@icloud.com>
2021-03-30 00:33:42 +0900

    takeRight

--- practice.js
+++ practice.js
@@ -1,13 +1,21 @@
-function uniq(array){
-const uniqArray=[];
-for(let i=0; i<array.length; i++){
-    const value=array[i];
-    if(!uniqArray.includes(value)){
-      uniqArray.push(value);  
-    }
+function takeRight(array,n=1){
+if(n===0){
+    return [];
 }
-return uniqArray;
+if(n>array.length){
+    return [...array];
 }
 
-const numbers=[2,1,2];
-console.log( uniq(numbers));
+if(n>array.length){
+return [...array];
+}
+
+const takenArray =[];
+for(let i=0; i<n; i++){
+    const indexFromRight = array.length - (1+i)
+    takenArray.unshift(array[indexFromRight]);
+}
+return takenArray;
+}
+
+console.log(takeRight([1,2,3]));
