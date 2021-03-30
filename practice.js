c7abca2538426977b911a7c2b16327f0f76b6528
satou <stuysyk1027@icloud.com>
2021-03-31 07:19:40 +0900

    slice

--- practice.js
+++ practice.js
@@ -1,9 +1,14 @@
-function tail (array){
-const newArray=[];
-for(let i=1; i<array.length; i++){
-    newArray.push(array[i]);
-}
-return newArray;
+function slice(array,start=0, end=array.length){
+  const slicedArray=[];
+for(let i=start; i<end; i++){
+    slicedArray.push(array[i]);
 }
 
-console.log(tail([1,2,3]) );
+  return slicedArray;
+}
+const Numbers=[10,20,30,40,50];
+const slicedNumbers=slice(Numbers,1,4);
+
+console.log(slicedNumbers);
+
+console.log(Numbers);
