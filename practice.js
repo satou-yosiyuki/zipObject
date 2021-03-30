d7ede9f5ef7941f86d1bd5f84953f05f9dd3afc9
satou <stuysyk1027@icloud.com>
2021-03-30 10:10:40 +0900

    take

--- practice.js
+++ practice.js
@@ -1,21 +1,15 @@
-function takeRight(array,n=1){
-if(n===0){
+function take(array, n=1){
+  if(n===0){
     return [];
-}
-if(n>array.length){
+  }
+  if(n>array.length){
     return [...array];
+  }
+  const takenValues=[];
+  for(let i=0; i<n; i++){
+takenValues.push(array[i]);
+  }
+  return takenValues;
 }
 
-if(n>array.length){
-return [...array];
-}
-
-const takenArray =[];
-for(let i=0; i<n; i++){
-    const indexFromRight = array.length - (1+i)
-    takenArray.unshift(array[indexFromRight]);
-}
-return takenArray;
-}
-
-console.log(takeRight([1,2,3]));
+console.log(take([1,2,3],5));
